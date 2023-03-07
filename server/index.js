602b89ff7fe96cd2b567af04ce87de79bb5eb3a4
Kejuan Harrison <bamaboi912@icloud.com>
2023-03-07 11:24:39 -0600

    Amended index.js to incorporate routes for CRUD processing

--- server/index.js
+++ server/index.js
@@ -3,15 +3,19 @@ const app = express();
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 
-//Middleware
+
 app.use(express.json());
 const cors = require("cors");
 
 app.use(cors());
-//define backend port
+
 const PORT = 3001
 
-//establish link to MongoDB
+const router = require("./Routes/patientRoutes");
+
+app.use("./patient", router);
+
+
 
 mongoose.connect("mongodb+srv://bamaboi912:Student1234!@cluster0.f4zp47m.mongodb.net/MERN_HEALTH_CAPSTONE?retryWrites=true&w=majority").then(() => console.log
 ("Connected To Database"))
