b17623412109590643e8d44cd6d0cc7139a955cd
Kejuan Harrison <bamaboi912@icloud.com>
2023-03-07 14:12:05 -0600

    created About page

---
+++ client/src/components/About.js
@@ -0,0 +1,26 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import troopers from "./images/pat.jpg"
+
+const About = () => {
+  return (
+    <div>
+
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h5">
+          Cape HealthCare
+        </Typography>
+        
+   <> <img src={troopers} alt="troopers"></img>
+   <p>Cape Cod Healthcare is the leading provider of healthcare services for residents and visitors of Cape Cod. With nearly 500 physicians, 5,400 employees and 790 volunteers, Cape Cod Healthcare is comprised of two acute care hospitals, the Cape’s leading provider of homecare and hospice services (VNA), a skilled nursing and rehabilitation facility, an assisted living facility, six urgent care centers and numerous health programs. For more information, visit the Cape Cod Healthcare website at www.capecodhealth.org. For the latest in relevant and credible healthcare news, visit www.capecodhealthnews.com.</p>
+   
+   
+   </>
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h6">
+          Established  &copy;2023
+        </Typography>
+      </Box>
+    </div>
+  );
+};
+export default About;
